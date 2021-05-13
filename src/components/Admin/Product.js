ba47860bc5d32d526c97922d0b0168a6d8f8414e
Sounak Dey <sounak.dey@iiitb.ac.in>
2021-05-13 16:24:15 +0530

    product imageFetch api

--- src/components/Admin/Product.js
+++ src/components/Admin/Product.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button, Container
@@ -8,6 +8,27 @@ import base_url from "../../api/bootapi";
 import {toast} from "react-toastify";
 
 const Product=({ product,update })=>{
+
+    const [image, setImage] = useState('');
+
+    useEffect(() => {
+        viewUploadedFile(product.product_id)
+    },[]);
+
+    const viewUploadedFile = (product_id ) => {
+        if (product_id !== null) {
+            axios.get(`${base_url}/products/image/${product_id}`, { responseType: 'blob' }).then(
+                response => {
+                if (response.data) {
+                    setImage(URL.createObjectURL(response.data))
+                } else {
+
+                }
+            }).catch(error => {
+                console.log("Error", error);
+            })
+        }
+    }
 
     const deleteProduct=(product_id)=>{
         axios.delete(`${base_url}/products/${product_id}`).then(
@@ -23,7 +44,7 @@ const Product=({ product,update })=>{
 
     return(
         <Card>
-            <CardImg top width="100%" alt="Card image cap" />
+            <CardImg top width="100%" src = {image} alt="Card image cap" />
             <CardBody className="text-center">
                 <CardTitle className="font-weight-bold">{product.name}</CardTitle>
                 <CardSubtitle>{product.price} <br/>
